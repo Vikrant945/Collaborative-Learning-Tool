1c1892be4f79eafa4cfaeb21659bddb658254224
Vikrant945 <112808094+Vikrant945@users.noreply.github.com>
2024-05-27 01:43:12 +0530

    Update client2.js

--- client2.js
+++ client2.js
@@ -216,12 +216,12 @@ const canvasRect = canvas.getBoundingClientRect();
 
     if (window.matchMedia("(max-width: 1276px)").matches) {
         // Adjust left property for smaller screens
-        leftValue = `${canvasRect.left + position.x - 245}px`; // Adjust this value according to your requirements
+        leftValue = `${canvasRect.left + position.x - 255}px`; // Adjust this value according to your requirements
     } 
     
     else if (window.matchMedia("(max-width: 1225px)").matches) {
         // Adjust left property for smaller screens
-        leftValue = `${canvasRect.left + position.x - 235}px`; // Adjust this value according to your requirements
+        leftValue = `${canvasRect.left + position.x - 245}px`; // Adjust this value according to your requirements
     } 
     else {
         // Default left property for larger screens
